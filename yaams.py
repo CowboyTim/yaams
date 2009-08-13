4508e865c19241f53f1475b755c649ea82d4ea33
Tim Aerts <aardbeiplantje@gmail.com>
2009-08-13 17:40:25 +0200

    - added a license - suppress deprecation warnings for python 2.6

--- yaams.py
+++ yaams.py
@@ -1,7 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/python -W ignore::DeprecationWarning
 
-# Yet Another Auto Mounter System (YAAMS)
+# License
 #
+# Yet Another Auto Mounter System (YAAMS) is distributed under the zlib/libpng
+# license, which is OSS (Open Source Software) compliant.
+#
+# Copyright (C) 2009 Tim Aerts
+#
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+#
+# Tim Aerts aardbeiplantje@gmail.com
+
+
 
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
@@ -11,8 +35,8 @@ import gobject
 from functools import partial
 from subprocess import call
 
-PIDPATH   = '/var/run/pymount.pid'
-LOGFILE   = '/var/log/pymount.log'
+PIDPATH   = '/var/run/yaams.pid'
+LOGFILE   = '/var/log/yaams.log'
 MOUNTBASE = '/media'
 
 logout = sys.stdout
@@ -33,7 +57,9 @@ def get_fstab_dev(dev_udi):
     if not fstab_entry_raw:
         return None
     
-    dev = dict(zip(('name', 'mountpoint', 'fstype', 'options', 'dumpfreq', 'passnum'), fstab_entry_raw.split()))
+    dev = dict(zip(('name', 'mountpoint', 'fstype', \
+                    'options', 'dumpfreq', 'passnum'), \
+                   fstab_entry_raw.split()))
     dev['uuid'] = uuid
     dev['uid'] = dev['gid'] = 0
     for o in dev['options'].split():
